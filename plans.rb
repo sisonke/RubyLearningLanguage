cf1f37d4712c4444e149e962dedf1bd4ce07e0f0
sisonke <culoe2@gmail.com>
2015-11-09 14:23:20 +0200

    plans

--- plans.rb
+++ plans.rb
@@ -2,6 +2,6 @@ require 'csv'
 plans = CSV.read('mobilephone.csv')
 puts plans
 
-CSV.foreach('mobilephone.csv') do |rows|
-	puts rows.inspect
+CSV.foreach('mobilephone.csv') do |row|
+	puts row.inspect
 end
